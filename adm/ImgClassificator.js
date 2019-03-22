ae09d3afa150c8670dbef8d2d019e0a1c6bd5d05
elexfreeman <elextraza@gmail.com>
2019-03-23 00:10:35 +0400

    Классификатор добави буфер

--- adm/ImgClassificator.js
+++ adm/ImgClassificator.js
@@ -5,21 +5,53 @@ const img_size = 256;
 /* загрузка картинки */
 document.getElementById("Upload").addEventListener("click", onUploadFile);
 
+/* делает буфер для вывода */
+function makeBuffer(source) {
+    let Buffer = new Uint8Array(img_size*img_size);;
+    let canvas = document.getElementById(source);  
+    var ctx = canvas.getContext('2d');
+    let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
+    let i = 0;
+    for (let X = 0; X < img_size; X++) {
+        for (let Y = 0; Y < img_size; Y++) {
+            Buffer[Y * img_size + X] = imageData.data[i]; //берем красный
+           /*  imageData.data[i + 1] = Buffer[Y * img_size + X];
+            imageData.data[i + 2] = Buffer[Y * img_size + X];
+            imageData.data[i + 3] = 255; */
+            i += 4;
+        }
+    }
+  
+    return Buffer;
+}
+
+/* событие загрузки файла */
 function onUploadFile(event) {
     console.log('upload');
+
+    /*  */
     var reader = new FileReader();
     reader.onload = function () {
         var output = document.getElementById('source');
         output.src = reader.result;
     };
+
+    /* читаем файл и ложим в картинку на канвасе */
     var selectedFile = document.getElementById('img_file').files[0];
     reader.readAsDataURL(selectedFile);
+    
     var image = document.getElementById('source');
 
+    /* вставляем в канвас оригинала */
     let canvas = document.getElementById('img_original');
     var ctx = canvas.getContext('2d');
     ctx.drawImage(image, 0, 0, img_size, img_size);
+
+    /* чето делаем */
+    let buffer = makeBuffer('img_original');
+    /* вставляем обработанный */
+    writeImg('img_class', buffer) 
 }
 
 /* ******************************************* */
@@ -36,6 +68,7 @@ function writeImg(id, Buffer) {
         return;
     }
 
+    let i = 0;
     for (let X = 0; X < img_size; X++) {
         for (let Y = 0; Y < img_size; Y++) {
             imageData.data[i] = Buffer[Y * img_size + X];
