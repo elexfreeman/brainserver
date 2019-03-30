444d3fef52743b741f6951cad0af6aebfa559d36
elexfreeman <elextraza@gmail.com>
2019-03-30 21:42:46 +0400

    добавил управление движения для куба

--- BotController.ts
+++ BotController.ts
@@ -1,21 +1,53 @@
-import { BotBrainClass } from './BotBrain';
+import { CubeBrainClass } from './CubeBrain';
 
 import { log } from './log';
 
 import { bots } from './Bots';
 
-export function BotController(socket) {
-    console.log('connect');
-    const bot = new BotBrainClass();
-    /* обработка событий текущего состояния */
-    socket.on('state message', (msg) => {
-        bot.onStateMessage(msg);
-    });
-    socket.on('img', (msg) => {
-        //bot.echo(msg);
-        socket.broadcast.emit('resend img', msg);
-    });
-    socket.on('disconnect', (msg) => {
-        bot.onDisconnect(msg);
-    });
+export default class BotController {
+
+    io: any;
+    constructor(io: any) {
+        this.io = io;
+    }
+
+
+    controller(socket) {
+        console.log('connect: ' + socket.id);
+        bots[socket.id] = new CubeBrainClass();
+
+        let timerId = setInterval(() => {
+            if(bots[socket.id]){
+                socket.emit('set_move', true);   
+                socket.emit('set_direction', bots[socket.id].getDirection());   
+            } else {
+                clearInterval(timerId);
+            }
+           
+        }, 1000);
+
+
+        /* обработка событий текущего состояния */
+        socket.on('state message', (msg) => {
+            bots[socket.id].onStateMessage(msg);
+        });
+        socket.on('img', (msg) => {
+            //bot.echo(msg);
+            socket.broadcast.emit('resend img', msg);
+        });
+        socket.on('drone_frame', (msg) => {
+            msg = msg.replace(/'/g, '"');
+            //console.log(msg);
+            try {
+                // console.log(JSON.parse(msg));
+            } catch (e) {
+                //console.log('JSON - error');
+            }
+        });
+        socket.on('disconnect', (msg) => {
+            bots[socket.id].onDisconnect(msg);
+            delete bots[socket.id];
+        });
+    }
+
 }
--- server.ts
+++ server.ts
@@ -3,14 +3,22 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-import { BotController } from './BotController';
+import BotController from './BotController';
 
 app.get('/', function (req, res) {
     res.json({});
 });
 
-io.on('connection', BotController);
+const port = 8080;
 
-http.listen(3000, function () {
-    console.log('listening on *:3000');
+const BCtrl = new BotController(io);
+
+
+
+io.on('connection', BCtrl.controller);
+
+
+
+http.listen(port, function () {
+    console.log('listening on *:' + port);
 });
